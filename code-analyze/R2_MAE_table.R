e7de12d0575e333e2053c2c0249498a7e59908ce
krenwick <katie.renwick@gmail.com>
2017-09-01 16:44:14 -0600

    makes new table with sumstats from LAI and GPP only optimization

--- code-analyze/R2_MAE_table.R
+++ code-analyze/R2_MAE_table.R
@@ -7,11 +7,16 @@ library(zoo)
 library(data.table)
 library(grid)
 library(gridExtra)
+library(xtable)
 
 # Set working directory
 setwd("~/Documents/SageParm")
 outname1 <- "mgl_disturb_summergrass1"
 outname2 <- "mgl_disturb_summergrass2"
+outnamegpp1 <- "monthg_summergrass1"
+outnamegpp2 <- "monthg_summergrass2"
+outnamelai1 <- "ml_summergrass1"
+outnamelai2 <- "ml_summergrass2"
 
 ################################################################################
 # Get GPP and LAI output from all model runs + orig data
@@ -37,6 +42,7 @@ df4 <- merge(df3,mod,by=c("Year","Month","Variable","Site"), all=T) %>%
   filter(Date>="2014-10-01")
 table(df4$Variable)
 
+################################################################################
 # Pull in data from the SUMMERGREEN model runs
 summer <- NULL 
 vars=c("mgpp","mrh","mra","mnee","mevap","maet","mlai")
@@ -108,84 +114,72 @@ all <- merge(ever,summer, by=c("Year","Month","Variable","Site")) %>%
 all$D <- as.yearmon(paste(all$Year, all$Month), "%Y %b")
 all$Date <- as.Date(all$D)
 
-# Pull GPP output from optim1 back in to R:
+# Pull GPP from optimization model runs into R:
 gpp1 <- fread(paste("Output_localruns/mgpp_",outname1,".txt",sep=""), header=T) %>%
-  dplyr::mutate(Year=Year+860) %>% 
-  filter(Year>=2014) %>%
-  gather(Month,Model, Jan:Dec) %>%
-  dplyr::mutate(Site=ifelse(Lon==-116.7486, "mbsec", "FIX")) %>%
-  dplyr::mutate(Site=ifelse(Lon==-116.7356, "losec", Site)) %>%
-  dplyr::mutate(Site=ifelse(Lon==-116.7132, "wbsec", Site)) %>%
-  dplyr::mutate(Site=ifelse(Lon==-116.7231, "h08ec", Site)) %>%
-  dplyr::mutate(Variable="GPP") %>%
-  dplyr::select(Year, Month,Site,Variable,Model) %>%
-  spread(Variable,Model) %>%
-  dplyr::select(Year,Month,Site,GPP) %>%
-  gather(Variable, Model, GPP)
-
-# Pull LAI output from optim1 back in to R:
-lai1 <- fread(paste("Output_localruns/mlai_",outname1,".txt",sep=""), header=T) %>%
-  dplyr::mutate(Year=Year+860) %>% 
-  filter(Year>=2014) %>%
-  gather(Month,Model, Jan:Dec) %>%
-  dplyr::mutate(Site=ifelse(Lon==-116.7486, "mbsec", "FIX")) %>%
-  dplyr::mutate(Site=ifelse(Lon==-116.7356, "losec", Site)) %>%
-  dplyr::mutate(Site=ifelse(Lon==-116.7132, "wbsec", Site)) %>%
-  dplyr::mutate(Site=ifelse(Lon==-116.7231, "h08ec", Site)) %>%
-  dplyr::mutate(Variable="LAI") %>%
-  dplyr::select(Year, Month,Site,Variable,Model) %>%
-  spread(Variable,Model) %>%
-  dplyr::select(Year,Month,Site,LAI) %>%
-  gather(Variable, Model, LAI)
-
-# Pull GPP output from optim2 back in to R:
+  gather(Month,Model, Jan:Dec)
 gpp2 <- fread(paste("Output_localruns/mgpp_",outname2,".txt",sep=""), header=T) %>%
+  gather(Month,Model2, Jan:Dec) 
+gpp3 <- fread(paste("Output_localruns/mgpp_",outnamegpp1,".txt",sep=""), header=T) %>%
+  gather(Month,GPP1, Jan:Dec)
+gpp4 <- fread(paste("Output_localruns/mgpp_",outnamegpp2,".txt",sep=""), header=T) %>%
+  gather(Month,GPP2, Jan:Dec)
+gpp5 <- fread(paste("Output_localruns/mgpp_",outnamelai1,".txt",sep=""), header=T) %>%
+  gather(Month,LAI1, Jan:Dec)
+gpp6 <- fread(paste("Output_localruns/mgpp_",outnamelai2,".txt",sep=""), header=T) %>%
+  gather(Month,LAI2, Jan:Dec)
+
+# Pull LAI from optimization model runs into R:
+lai1 <- fread(paste("Output_localruns/mlai_",outname1,".txt",sep=""), header=T) %>%
+  gather(Month,Model, Jan:Dec)
+lai2 <- fread(paste("Output_localruns/mlai_",outname2,".txt",sep=""), header=T) %>%
+  gather(Month,Model2, Jan:Dec)
+lai3 <- fread(paste("Output_localruns/mlai_",outnamegpp1,".txt",sep=""), header=T) %>%
+  gather(Month,GPP1, Jan:Dec)
+lai4 <- fread(paste("Output_localruns/mlai_",outnamegpp2,".txt",sep=""), header=T) %>%
+  gather(Month,GPP2, Jan:Dec)
+lai5 <- fread(paste("Output_localruns/mlai_",outnamelai1,".txt",sep=""), header=T) %>%
+  gather(Month,LAI1, Jan:Dec)
+lai6 <- fread(paste("Output_localruns/mlai_",outnamelai2,".txt",sep=""), header=T) %>%
+  gather(Month,LAI2, Jan:Dec)
+
+gpps <- Reduce(function(...) merge(..., all=TRUE), list(gpp1,gpp2,gpp3,gpp4,gpp5,gpp6)) %>%
   dplyr::mutate(Year=Year+860) %>% 
   filter(Year>=2014) %>%
-  gather(Month,Model2, Jan:Dec) %>%
   dplyr::mutate(Site=ifelse(Lon==-116.7486, "mbsec", "FIX")) %>%
   dplyr::mutate(Site=ifelse(Lon==-116.7356, "losec", Site)) %>%
   dplyr::mutate(Site=ifelse(Lon==-116.7132, "wbsec", Site)) %>%
   dplyr::mutate(Site=ifelse(Lon==-116.7231, "h08ec", Site)) %>%
   dplyr::mutate(Variable="GPP") %>%
-  dplyr::select(Year, Month,Site,Variable,Model2) %>%
-  spread(Variable,Model2) %>%
-  dplyr::select(Year,Month,Site,GPP) %>%
-  gather(Variable, Model2, GPP)
+  dplyr::select(-Lon,-Lat) 
 
-# Pull LAI output from optim2 back in to R:
-lai2 <- fread(paste("Output_localruns/mlai_",outname2,".txt",sep=""), header=T) %>%
+lais <- Reduce(function(...) merge(..., all=TRUE), list(lai1,lai2,lai3,lai4,lai5,lai6)) %>%
   dplyr::mutate(Year=Year+860) %>% 
   filter(Year>=2014) %>%
-  gather(Month,Model2, Jan:Dec) %>%
   dplyr::mutate(Site=ifelse(Lon==-116.7486, "mbsec", "FIX")) %>%
   dplyr::mutate(Site=ifelse(Lon==-116.7356, "losec", Site)) %>%
   dplyr::mutate(Site=ifelse(Lon==-116.7132, "wbsec", Site)) %>%
   dplyr::mutate(Site=ifelse(Lon==-116.7231, "h08ec", Site)) %>%
   dplyr::mutate(Variable="LAI") %>%
-  dplyr::select(Year, Month,Site,Variable,Model2) %>%
-  spread(Variable,Model2) %>%
-  dplyr::select(Year,Month,Site,LAI) %>%
-  gather(Variable, Model2, LAI)
+  dplyr::select(-Lon,-Lat) 
 
 # Merge with flux data
-GPPall <- merge(gpp1,all, by=c("Year","Month","Site","Variable")) %>%
-  merge(.,gpp2, by=c("Year","Month","Site","Variable")) %>%
+GPPall <- merge(gpps,all, by=c("Year","Month","Site","Variable")) %>%
   filter(Variable=="GPP") %>%
-  gather(Source, GPP, c(Tower,Model,Model2,Evergreen,Summergreen, Raingreen)) %>%
+  select(-MODIS) %>%
+  gather(Source, GPP, Model:Tower) %>%
   mutate(Source=factor(Source, levels=c("Tower","Summergreen","Evergreen","Raingreen",
-                                        "Model","Model2"), ordered=T)) %>%
-  
-  mutate(Site=factor(Site, levels=c("wbsec","losec","h08ec","mbsec"), ordered=T))
+                                        "Model","Model2","GPP1","GPP2",
+                                        "LAI1","LAI2"), ordered=T)) %>%
+   mutate(Site=factor(Site, levels=c("wbsec","losec","h08ec","mbsec"), ordered=T))
 
 # Merge with modis data
-LAIall <- merge(lai1,all, by=c("Year","Month","Site","Variable")) %>%
-  merge(.,lai2, by=c("Year","Month","Site","Variable")) %>%
+LAIall <- merge(lais,all, by=c("Year","Month","Site","Variable")) %>%
   filter(Variable=="LAI") %>%
-  gather(Source, LAI, c(MODIS,Model,Model2,Evergreen,Summergreen,Raingreen)) %>%
+  select(-Tower) %>%
+  gather(Source, LAI, Model:MODIS) %>%
   mutate(Source=factor(Source, levels=c("MODIS","Summergreen","Evergreen","Raingreen",
-                                        "Model","Model2"), ordered=T)) %>%
-  filter(Source!="Evergreen") %>%
+                                        "Model","Model2","GPP1","GPP2",
+                                        "LAI1","LAI2"), ordered=T)) %>%
   mutate(Site=factor(Site, levels=c("wbsec","losec","h08ec","mbsec"), ordered=T))
 
 ################################################################################
@@ -194,30 +188,35 @@ LAIall <- merge(lai1,all, by=c("Year","Month","Site","Variable")) %>%
 # GPP------------------------------------------
 g1 <- GPPall %>%
   filter(Date>="2014-10-01"&Date<="2016-09-01") %>%
-  select(-Variable,-MODIS) %>%
+  select(-Variable) %>%
   spread(Source,GPP)
 head(g1)
 
 # ACROSS ALL 4 SITES:
 # Get R2:
-Model <- c("Original","NewParm","NewPhen")
+Model <- c("Original","NewParm","NewPhen","Mod1GPP","Mod2GPP","Mod1LAI","Mod2LAI")
 raingreen <- summary(lm(data=g1, Tower~Raingreen))$adj.r.squared #.71
 evergreen <- summary(lm(data=g1, Tower~Evergreen))$adj.r.squared #.69
 summergreen <- summary(lm(data=g1, Tower~Summergreen))$adj.r.squared #.66
 NewParm <- summary(lm(data=g1, Tower~Model))$adj.r.squared #.72
 NewPhen <- summary(lm(data=g1, Tower~Model2))$adj.r.squared #.84
-Value <- c(summergreen,NewParm,NewPhen)
+NewParmGPP <- summary(lm(data=g1, Tower~GPP1))$adj.r.squared #.72
+NewPhenGPP <- summary(lm(data=g1, Tower~GPP2))$adj.r.squared #.84
+NewParmLAI <- summary(lm(data=g1, Tower~LAI1))$adj.r.squared #.72
+NewPhenLAI <- summary(lm(data=g1, Tower~LAI2))$adj.r.squared #.84
+Value <- c(summergreen,NewParm,NewPhen,NewParmGPP,NewPhenGPP,NewParmLAI,NewPhenLAI)
 stats1 <- cbind.data.frame(Model,Value) %>%
   mutate(Metric="R2", Variable="GPP")
 
 # Get MAE:
-Value <- g1 %>% mutate_at(vars(Summergreen:Model2), funs(.-Tower)) %>%
+Value <- g1 %>% select(Date,Site,Tower:LAI2) %>%
+  mutate_at(vars(Summergreen:LAI2), funs(.-Tower)) %>%
   na.omit() %>%
-  mutate_at(vars(Summergreen:Model2), funs(abs(.))) %>%
+  mutate_at(vars(Summergreen:LAI2), funs(abs(.))) %>%
   group_by(Site) %>%
-  summarise_at(vars(Summergreen:Model2), funs(sum(.))) %>%
+  summarise_at(vars(Summergreen:LAI2), funs(sum(.))) %>%
   ungroup() %>%
-  summarise_at(vars(Summergreen:Model2), funs(mean(.))) %>%
+  summarise_at(vars(Summergreen:LAI2), funs(mean(.))) %>%
   select(-Evergreen, -Raingreen)
 Value
 Value <- as.numeric(Value)
@@ -227,7 +226,7 @@ stats2 <- cbind.data.frame(Model,Value) %>%
 # LAI------------------------------------------
 l1 <- LAIall %>%
   filter(Date>="2014-10-01"&Date<="2016-09-01") %>%
-  select(-Variable,-Tower) %>%
+  select(-Variable) %>%
   spread(Source,LAI)
 head(l1)
 
@@ -236,19 +235,24 @@ head(l1)
 summergreen <- summary(lm(data=l1, MODIS~Summergreen))$adj.r.squared #.29
 mod1 <- summary(lm(data=l1, MODIS~Model))$adj.r.squared #.32
 mod2 <- summary(lm(data=l1, MODIS~Model2))$adj.r.squared #.75
-Value <- c(summergreen,mod1,mod2)
+NewParmGPP <- summary(lm(data=l1, MODIS~GPP1))$adj.r.squared #.72
+NewPhenGPP <- summary(lm(data=l1, MODIS~GPP2))$adj.r.squared #.84
+NewParmLAI <- summary(lm(data=l1, MODIS~LAI1))$adj.r.squared #.72
+NewPhenLAI <- summary(lm(data=l1, MODIS~LAI2))$adj.r.squared #.84
+Value <- c(summergreen,mod1,mod2,NewParmGPP,NewPhenGPP,NewParmLAI,NewPhenLAI)
 stats3 <- cbind.data.frame(Model,Value) %>%
   mutate(Metric="R2",Variable="LAI")
 
 # Get MAE:
-Value <- l1 %>% mutate_at(vars(Summergreen:Model2), funs(.-MODIS)) %>%
+Value <- l1 %>% select(Date,Site,MODIS:LAI2) %>%
+  mutate_at(vars(Summergreen:LAI2), funs(.-MODIS)) %>%
   na.omit() %>%
-  mutate_at(vars(Summergreen:Model2), funs(abs(.))) %>%
+  mutate_at(vars(Summergreen:LAI2), funs(abs(.))) %>%
   group_by(Site) %>%
-  summarise_at(vars(Summergreen:Model2),funs(sum(.))) %>% 
+  summarise_at(vars(Summergreen:LAI2), funs(sum(.))) %>%
   ungroup() %>%
-  summarise_at(vars(Summergreen:Model2),funs(mean(.))) %>%
-  select(-Raingreen)
+  summarise_at(vars(Summergreen:LAI2), funs(mean(.))) %>%
+  select(-Raingreen,-Evergreen)
 Value <- as.numeric(Value)
 stats4 <- cbind.data.frame(Model,Value) %>%
   mutate(Metric="MAE",Variable="LAI")
@@ -258,18 +262,20 @@ stats4 <- cbind.data.frame(Model,Value) %>%
 ################################################################################
 R2 <- rbind.data.frame(stats1,stats3) %>%
   mutate_if(is.numeric, funs(round(.,2))) %>%
-  mutate(Model=factor(Model, levels=c("Original","NewParm","NewPhen"), ordered=T,
-                      labels=c("Original","Optimal Parameters","New Phenology")))
+  spread(Variable,Value) %>% select(-Metric) 
 MAE <- rbind.data.frame(stats2,stats4) %>%
   mutate_if(is.numeric, funs(round(.,2))) %>%
+  spread(Variable,Value) %>% select(-Metric) %>%
+  rename(GPP2=GPP, LAI2=LAI)
+
+# Original table:
+both <- merge(R2,MAE, by="Model") %>%
+  filter(Model=="Original"|Model=="NewParm"|Model=="NewPhen") %>%
   mutate(Model=factor(Model, levels=c("Original","NewParm","NewPhen"), ordered=T,
-                      labels=c("Original","Optimal Parameters","New Phenology")))
-
-R2b <- R2 %>% spread(Variable,Value) %>% select(-Metric)
-MAEb <- MAE %>% spread(Variable,Value) %>% select(-Metric,-Model)
-both <- cbind(R2b,MAEb) 
-
-#########################
+  labels=c("Original","Optimal Parameters","New Phenology"))) %>%
+  arrange(Model)
+ 
+#names(both) <- c("Optimization Data","Model","GPP","LAI","GPP","LAI")
 # Make both into a pretty table
 t <- xtable(both)
 print(t,
@@ -277,6 +283,98 @@ print(t,
       include.rownames=FALSE,
       type="latex",
       booktabs=T,
-      #digits(tbl) <- c(0,1,1,1,1,1),
       file="figures/R2MAE.tex")
+########################################################################
+# Attempt at bigger more complicated table
+########################################################################
+both2 <- merge(R2,MAE, by="Model") %>%
+ mutate(Model=factor(Model, levels=c("Original","NewParm","NewPhen","Mod1GPP",
+                                      "Mod2GPP","Mod1LAI","Mod2LAI"), ordered=T,
+labels=c("Original","Optimal Parameters","New Phenology",
+         "Optimal Parameters","New Phenology",
+         "Optimal Parameters","New Phenology"))) %>%
+  arrange(Model)
+
+optim <- c(NA,"GPP and LAI",NA,"Just GPP",NA,"Just LAI",NA)
+b3 <- cbind(optim,both2) %>%
+  rename(`Optimization Data`=optim) %>%
+  mutate(Model=as.character(Model)) # must change from factor to bold
+b3
+names(b3) <- c("Optimization Data","Model","GPP","LAI","GPP","LAI")
+# Make a pretty table
+b3[1,2:6] <- paste0("BOLD", b3[1,2:6])
+# Gah! lose trailing zero. I'm manually adding in latex file
+
+bold.somerows <- 
+  function(x) gsub('BOLD(.*)',paste('\\\\textbf{\\1}'),x)
+t2 <- xtable(b3)
+# print(t2,
+#       only.contents=TRUE,
+#       include.rownames=FALSE,
+#       type="latex",
+#       booktabs=T,
+#       hline.after=c(0,1,3,5),
+#       sanitize.text.function = bold.somerows,
+#       file="figures/R2MAE2.tex")
+
+################################################################################
+# Make some plots to see if hit peak
+################################################################################
+g2 <- GPPall %>%
+  filter(Date>="2014-10-01"&Date<="2016-09-01")
+g3 <- filter(g2, Source=="Tower"|Source=="Model2"|Source=="GPP2"|Source=="LAI2")
+# Plot GPP:
+g1 <- ggplot(data=g3, aes(x=Date, y=GPP)) +
+  #geom_point() +
+  geom_line(aes(color=Source, linetype=Source)) +
+  # scale_linetype_manual(name="Source",
+  #                       breaks=c("Tower","Model","Model2"),
+  #                       labels=c("Tower","Optimal Parameters","New Phenology"),
+  #                       values=c("solid", "dashed","dashed"))+
+  # scale_color_manual(name="Source",
+  #                    breaks=c("Tower","Model","Model2"),
+  #                    labels=c("Tower","Optimal Parameters","New Phenology"),
+  #                    values=c("black","cyan3","deeppink3")) +
+  facet_wrap(~Site) +
+  scale_x_date(date_breaks = "2 months",date_labels = "%b %y") +
+  #geom_text(data = ann_text_gpp,aes(x=Date, y=GPP,label=lab)) +
+  xlab("Date") +
+  ylab(expression(GPP~(kgC~m^{-2}))) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1),
+        strip.background = element_blank(),
+        strip.text = element_blank(),
+        panel.grid.minor = element_blank(),
+        # panel.border = element_rect(colour = "black"),
+        legend.position = "top") 
+g1
+
+
+# Plot LAI:
+l1 <- ggplot(data=LAIall, aes(x=Date, y=LAI)) +
+  #geom_point() +
+  geom_line(aes(color=Source, linetype=Source)) +
+  scale_linetype_manual(name="Source",
+                        breaks=c("Tower","Model","Model2"),
+                        labels=c("Tower","Optimal Parameters","New Phenology"),
+                        values=c("solid", "dashed","dashed"))+
+  scale_color_manual(name="Source",
+                     breaks=c("Tower","Model","Model2"),
+                     labels=c("Tower","Optimal Parameters","New Phenology"),
+                     values=c("black","cyan3","deeppink3")) +
+  facet_wrap(~Site) +
+  scale_x_date(date_breaks = "2 months",date_labels = "%b %y") +
+  geom_text(data = ann_text_lai,aes(x=Date, y=LAI,label=lab)) +
+  xlab("Date") +
+  ylab("LAI") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1),
+        strip.background = element_blank(),
+        strip.text = element_blank(),
+        panel.grid.minor = element_blank(),
+        # panel.border = element_rect(colour = "black"),
+        legend.position = "top") 
+
+l1
+
+
+
   
