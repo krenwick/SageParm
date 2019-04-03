0b745a091d049aa8ee7d57a2051a89c6f9d029ec
krenwick <katie.renwick@gmail.com>
2019-04-03 07:06:09 -0600

    add code looking at parm estimates from diff optim runs

--- code-analyze/table_parm_estimates.R
+++ code-analyze/table_parm_estimates.R
@@ -66,3 +66,76 @@ print(t,
       booktabs=T,
       sanitize.text.function = identity,
       file="figures/parm_estimates.tex")
+
+################################################################################
+# Try adding columns for parm vals from different optimiation runs (LAI vs GPP)
+################################################################################
+origLAI <- "HyaliteOutput/DE1parimage_ml_summergrass.RData"
+origGPP <- "HyaliteOutput/DE1parimage_mg_summergrass.RData"
+newLAI <- "HyaliteOutput/NewPhenImage_ml_summergrass.RData"
+newGPP <- "HyaliteOutput/NewPhenImage_mg_summergrass.RData"
+
+# List parameters estimated in round 1:
+parms <- c("SLA","latosa","ltor_max","root_up","pstemp_min",
+           "pstemp_low","pstemp_max","GDD5")
+# Get parameter values origLAI:
+load(origLAI)
+parsorigLAI <- cbind.data.frame(parms,orig,DE1$optim$bestmem) %>%
+  rename(OrigLAI=`DE1$optim$bestmem`)
+
+# Get parameter values origGPP:
+load(origGPP)
+parsorigGPP <- cbind.data.frame(parms,orig,DE1$optim$bestmem) %>%
+  rename(OrigGPP=`DE1$optim$bestmem`)
+
+# Get parameter values from optim 2:
+parms <- c("SLA","latosa","ltor_max","root_up","phen_winter","pstemp_low",
+           "pstemp_min","apheng","aphensage","GDD5g")
+load(newLAI)
+parsnewLAI <- cbind.data.frame(parms,DE1$optim$bestmem) %>%
+  rename(NewLAI=`DE1$optim$bestmem`)
+load(newGPP)
+parsnewGPP <- cbind.data.frame(parms,DE1$optim$bestmem) %>%
+  rename(NewGPP=`DE1$optim$bestmem`)
+
+# Merge all into 1 table:
+all2 <- merge(pars1,pars2,by="parms",all=T) %>%
+  merge(.,parsorigLAI, by=c("parms","orig"), all=T) %>%
+  merge(.,parsorigGPP, by=c("parms","orig"), all=T) %>%
+  merge(.,parsnewLAI, by=c("parms"), all=T) %>%
+  merge(.,parsnewGPP, by=c("parms"), all=T) %>%
+  mutate(Parameter=factor(parms, levels=c("SLA","ltor_max","root_up","GDD5",
+                                          "latosa","pstemp_min","pstemp_low","pstemp_max","phen_winter",
+                                          "aphensage","apheng","GDD5g"), ordered=T, labels=c("sla",
+                                                                                             "ltor\\textsubscript{max}","root\\textsubscript{up}",
+                                                                                             "GDD\\textsubscript{5,sage}","latosa",
+                                                                                             "pstemp\\textsubscript{min}","pstemp\\textsubscript{low}",
+                                                                                             "pstemp\\textsubscript{max}","phen\\textsubscript{winter}",
+                                                                                             "phen\\textsubscript{max,sage}","phen\\textsubscript{max,grass}",
+                                                                                             "GDD\\textsubscript{5,grass}"))) %>%
+  mutate_at(vars(orig:NewGPP),funs(ifelse(abs(.)>1,round(.),round(.,1)))) %>%
+  dplyr::select(Parameter,Original=orig,`Opt Param GPPLAI`=Estimate,
+                `Optimal Parameters GPP`=OrigGPP,
+                `Optimal Parameters LAI`=OrigLAI,
+                `New Phenology GPPLAI`=Estimate2,
+                `New Phenology GPP`=NewGPP,
+                `New Phenology LAI`=NewLAI)
+all2
+
+# Add original value for GDD5 grass:
+all2[11,2] <- 100
+
+# pretty table
+digits <- matrix
+t <- xtable(all, digits=1)
+print(t)
+matrix(c(0,0,1,0,0,0,1,0,0,0,0,1), nrow = 12, ncol = 7)
+digits(t) <- matrix(c(0,0,1,0,0,0,1,0,0,0,0,1), nrow = 12, ncol(t)+1)
+print(t,
+      only.contents=TRUE,
+      include.rownames=FALSE,
+      type="latex",
+      booktabs=T,
+      sanitize.text.function = identity,
+      file="figures/LatexTables/parm_estimates_bigtable.tex")
+
